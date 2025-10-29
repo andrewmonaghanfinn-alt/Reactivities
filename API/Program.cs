e002491e740ccbc36270d5c4ff95e85d8ceaaf68
Finn Andrew-Monaghan <andrewmonaghanfinn@gmail.com>
2025-10-29 11:28:33 +0000

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -11,10 +11,15 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+
+//the variable x is used as a shorthand for options
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:3000", "http://localhost:3000"));
+
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
