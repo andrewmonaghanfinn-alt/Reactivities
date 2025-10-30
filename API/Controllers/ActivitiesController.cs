f709c49ff9520548013adc294d7db7c15f6cafba
Finn Andrew-Monaghan <andrewmonaghanfinn@gmail.com>
2025-10-30 13:54:11 +0000

    End of section 4

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,28 +1,49 @@
 using System;
+using Application.Activities.Commands;
+using Application.Activities.Queries;
 using Domain;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Persistence;
+
 
 namespace API.Controllers;
 //primary constructor used below, essentially the same as declaring a constructor that takes an AppDbContext as an argument as assigns it
 //to an AppDbContext member variable but more concise
-public class ActivitiesController(AppDbContext context) : BaseApiController
+public class ActivitiesController : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        return await context.Activities.ToListAsync();
+        //sends a Query member object from the GetActivityList class which sends queries and handles the response
+        return await Mediator.Send(new GetActivityList.Query());
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivityDetail(string id)
     {
-        //find activity record based on primary key id
-        var activity = await context.Activities.FindAsync(id);
-        
-        if (activity == null) return NotFound();
+        return await Mediator.Send(new GetActivityDetails.Query { Id = id });
+    }
 
-        return activity;
+    [HttpPost]
+    public async Task<ActionResult<string>> CreateActivity(Activity activity)
+    {
+        return await Mediator.Send(new CreateActivity.Command { Activity = activity });
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> EditActivity(Activity activity)
+    {
+        await Mediator.Send(new EditActivity.Command { Activity = activity });
+        //NOTE: we might need to return something for adapting this to CAM data fix to confirm changes 
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteActivity(string id)
+    {
+        await Mediator.Send(new DeleteActivity.Command { Id = id });
+
+        return Ok();
     }
 }
