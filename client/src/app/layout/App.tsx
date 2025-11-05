bdcc6cc85907d37f7af2121c5f4490e3457728bd
Finn Andrew-Monaghan <andrewmonaghanfinn@gmail.com>
2025-11-05 09:11:09 +0000

    End of section 6

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,8 +1,8 @@
-import { Box, Container, CssBaseline } from "@mui/material";
-import axios from "axios";
-import { useEffect, useState } from "react";
+import { Box, Container, CssBaseline, Typography } from "@mui/material";
+import { useState } from "react";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import { useActivities } from "../../lib/hooks/useActivities";
 //import ActivityCard from "../../features/activities/dashboard/ActivityCard";
 
 function App() {
@@ -11,26 +11,16 @@ function App() {
 
   //IMPORTANT: useState literally just returns a 2 element array with references to a variable and a function
   //the function is pretty much just a setter method for the variable, no other functionality occurs within this method
-  const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<
     Activity | undefined
   >(undefined);
   const [editMode, setEditMode] = useState(false);
-
-  //useEffect triggers a function when the above mounts
-  //we are using a fetch to grab a promise that holds the information returned from the API
-  // then chaining .then()'s to get the JSON version and then getting the data
-  //from the JSON response as a parameter to setActivities()
-  useEffect(() => {
-    axios
-      .get<Activity[]>("https://localhost:5001/api/activities/")
-      .then((response) => setActivities(response.data));
-  }, []);
+  const { activities, isPending } = useActivities();
 
   //takes in the id of an activity and finds the corresponding activity in the activities[]
   //sets selectedActivity to the queried value if possible
   const handleSelectActivity = (id: string) => {
-    setSelectedActivity(activities.find((x) => x.id === id));
+    setSelectedActivity(activities!.find((x) => x.id === id));
   };
 
   const handleCancelSelectActivity = () => {
@@ -50,37 +40,24 @@ function App() {
     setEditMode(false);
   };
 
-  const handleSubmitForm = (activity: Activity) => {
-    if (activity.id) {
-      setActivities(activities.map(x => x.id === activity.id ? activity : x))
-    } else {
-      const newActivity = {...activity, id: activities.length.toString()}
-      setSelectedActivity(newActivity);
-      setActivities([...activities, newActivity])
-    }
-    setEditMode(false);
-  }
-
-  const handleDelete = (id: string) => {
-    setActivities(activities.filter(x => x.id !== id))
-  }
-
   return (
-    <Box sx={{ backgroundColor: "#eeeeee" }}>
+    <Box sx={{ backgroundColor: "#eeeeee", minHeight: "100vh" }}>
       <CssBaseline />
       <NavBar openForm={handleOpenForm} />
       <Container maxWidth="xl" sx={{ marginTop: 3 }}>
-        <ActivityDashboard
-          activities={activities}
-          selectActivity={handleSelectActivity}
-          cancelSelectActivity={handleCancelSelectActivity}
-          selectedActivity={selectedActivity}
-          editMode={editMode}
-          openForm={handleOpenForm}
-          closeForm={handleFormClose}
-          submitForm={handleSubmitForm}
-          deleteActivity={handleDelete}
-        />
+        {!activities || isPending ? (
+          <Typography>Loading...</Typography>
+        ) : (
+          <ActivityDashboard
+            activities={activities}
+            selectActivity={handleSelectActivity}
+            cancelSelectActivity={handleCancelSelectActivity}
+            selectedActivity={selectedActivity}
+            editMode={editMode}
+            openForm={handleOpenForm}
+            closeForm={handleFormClose}
+          />
+        )}
       </Container>
     </Box>
   );
