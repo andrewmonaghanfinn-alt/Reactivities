cae7cff49fd53133237c7e3de1be444d167597ec
Finn Andrew-Monaghan <andrewmonaghanfinn@gmail.com>
2025-11-05 11:45:33 +0000

    End of section 7

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,63 +1,14 @@
-import { Box, Container, CssBaseline, Typography } from "@mui/material";
-import { useState } from "react";
+import { Box, Container, CssBaseline } from "@mui/material";
 import NavBar from "./NavBar";
-import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import { useActivities } from "../../lib/hooks/useActivities";
-//import ActivityCard from "../../features/activities/dashboard/ActivityCard";
+import { Outlet } from "react-router";
 
 function App() {
-  //useState allows a component to store and update information (state)
-  // const [the thing being stored, the function that can update it] = useState([]);
-
-  //IMPORTANT: useState literally just returns a 2 element array with references to a variable and a function
-  //the function is pretty much just a setter method for the variable, no other functionality occurs within this method
-  const [selectedActivity, setSelectedActivity] = useState<
-    Activity | undefined
-  >(undefined);
-  const [editMode, setEditMode] = useState(false);
-  const { activities, isPending } = useActivities();
-
-  //takes in the id of an activity and finds the corresponding activity in the activities[]
-  //sets selectedActivity to the queried value if possible
-  const handleSelectActivity = (id: string) => {
-    setSelectedActivity(activities!.find((x) => x.id === id));
-  };
-
-  const handleCancelSelectActivity = () => {
-    setSelectedActivity(undefined);
-  };
-
-  const handleOpenForm = (id?: string) => {
-    if (id) {
-      handleSelectActivity(id);
-    } else {
-      handleCancelSelectActivity();
-    }
-    setEditMode(true);
-  };
-
-  const handleFormClose = () => {
-    setEditMode(false);
-  };
-
   return (
     <Box sx={{ backgroundColor: "#eeeeee", minHeight: "100vh" }}>
       <CssBaseline />
-      <NavBar openForm={handleOpenForm} />
+      <NavBar />
       <Container maxWidth="xl" sx={{ marginTop: 3 }}>
-        {!activities || isPending ? (
-          <Typography>Loading...</Typography>
-        ) : (
-          <ActivityDashboard
-            activities={activities}
-            selectActivity={handleSelectActivity}
-            cancelSelectActivity={handleCancelSelectActivity}
-            selectedActivity={selectedActivity}
-            editMode={editMode}
-            openForm={handleOpenForm}
-            closeForm={handleFormClose}
-          />
-        )}
+        <Outlet />
       </Container>
     </Box>
   );
